a9a181c5a975d6c8a0880475e0cca72b5eff2042
Chih-Wei <chihweiliu1993@gmail.com>
2019-02-15 12:44:40 +0100

    Added dynamic background loading functionality

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,15 +1,46 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
 import "./styles/TodoContainer.css";
+import background1 from "../images/background1.jpg";
+import background2 from "../images/background2.jpg";
+import background3 from "../images/background3.jpg";
+import background4 from "../images/background4.jpg";
+import background5 from "../images/background5.jpg";
+import background6 from "../images/background6.jpg";
 
-const TodoContainer = (props) => {
-  return (
-    <div className="twelve wide column todoContainer">
-      <div className="ui container">
-        {props.children}
+
+class TodoContainer extends Component {
+  render() {
+    let styles;
+    if(this.props.auth && this.props.category && this.props.category !== "Inbox") {
+      const project = this.props.auth.projects.filter(project => project.name === this.props.category);
+      const backgroundImage = project[0].image;
+      if(backgroundImage === null) {
+        styles = { backgroundColor: "teal" };
+      } else {
+        const map = { background1, background2, background3, background4, background5, background6 };
+        // map the string backgroundImage to the variable for the background  import
+        const background = map[backgroundImage];
+        styles = { backgroundImage: `url(${background})`, backgroundSize: "cover" };
+      }
+    } else {
+      styles = { backgroundColor: "teal" };
+    }
+
+    return (
+      <div className="twelve wide column todoContainer" style={styles}>
+        <div className="ui container">
+          {this.props.children}
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 };
-
-export default TodoContainer;
+const mapStateToProps = (state) => {
+  return {
+    auth: state.auth,
+    category: state.category
+  }
+};
+export default connect(mapStateToProps) (TodoContainer);
