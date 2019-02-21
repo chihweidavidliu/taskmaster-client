916550b16f6e0feb5c4cc9f35107105e1b6bd1bb
Chih-Wei <chihweiliu1993@gmail.com>
2019-02-21 17:36:54 +0100

    added date and time to navbar

--- src/components/Dashboard/Navbar.js
+++ src/components/Dashboard/Navbar.js
@@ -1,24 +1,37 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 
 import * as actions from "actions";
-import "components/styles/Navbar.css"
-
+import "components/styles/Navbar.css";
 
 class Navbar extends Component {
   renderUsername = () => {
-    if(!this.props.auth) {
-      return ""
+    if (!this.props.auth) {
+      return "";
     }
-    return this.props.auth.name
-  }
+    return this.props.auth.name;
+  };
 
   render() {
     return (
-      <div className="ui menu fluid stackable" style={{backgroundColor: "#05A89E"}}>
+      <div className="ui menu fluid stackable" style={{ backgroundColor: "#05A89E" }}>
         <div className="item">
-          <Link to="/dashboard"><h1>TaskMaster</h1></Link>
+          <Link to="/dashboard">
+            <h1>TaskMaster</h1>
+          </Link>
+        </div>
+        <div className="item">
+          <p>
+            {new Date().toLocaleString([], {
+              year: "2-digit",
+              month: "2-digit",
+              day: "2-digit",
+              hour: "2-digit",
+              minute: "2-digit",
+              hour12: false
+            })}
+          </p>
         </div>
         <div className="item">
           <p>Signed in as {this.renderUsername()}</p>
@@ -27,13 +40,16 @@ class Navbar extends Component {
           <a href="/api/logout">Sign-out</a>
         </div>
       </div>
-    )
+    );
   }
 }
 
 const mapStateToProps = (state) => {
   return {
     auth: state.auth
-  }
-}
-export default connect(mapStateToProps, actions) (Navbar);
+  };
+};
+export default connect(
+  mapStateToProps,
+  actions
+)(Navbar);
